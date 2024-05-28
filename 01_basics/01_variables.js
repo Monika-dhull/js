38fe67879184d59767543c2496bb9ca4745349da
Monika-dhull <97228002+Monika-dhull@users.noreply.github.com>
2024-05-28 09:00:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 13543
+let accountEmail = "snckjdbf@gmaul.accountEmail"
+// var jdfdskj = 2345  we not use it now.
+// ndefinr = 1324 its not correct way
+// let sjdnekf; it will be undefined
+
+console.log(accountId);
+console.table([accountId, accountEmail])
+
+
